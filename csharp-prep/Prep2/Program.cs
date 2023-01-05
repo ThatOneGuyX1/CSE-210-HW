05d15ab4ad91be248dfb2cd48a1ca178677b09fa
ThatOneGuyX1 <iansearle0@gmail.com>
2023-01-05 11:26:14 -0700

    Finished Prep 2 assignemnt

--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -4,6 +4,40 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep2 World!");
+        string grade;
+        Console.Write("What is your grade perctange? ");
+        string gradePercentString = Console.ReadLine();
+        float gradePercentFloat = float.Parse(gradePercentString);
+        if (gradePercentFloat >= 90)
+        {
+            grade = "A";
+        }
+        else if ( gradePercentFloat >= 80 && gradePercentFloat < 90)
+        {
+            grade = "B";
+        }
+        else if ( gradePercentFloat >= 70 && gradePercentFloat < 80)
+        {
+            grade = "C";
+        }
+        else if ( gradePercentFloat >= 60 && gradePercentFloat < 70)
+        {
+            grade = "D";
+        }
+        else 
+        {
+            grade = "F";
+        }
+
+        if (gradePercentFloat > 70)
+        {
+            Console.Write($"You passed with a grade of {grade}");
+        }
+        else
+        {
+            Console.Write($"You did not pass. Your grade is {grade}");
+        }
+
+    
     }
 }
