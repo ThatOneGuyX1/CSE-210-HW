166a47599be9fe3f15c14110ca7234686b0a5208
ThatOneGuyX1 <iansearle0@gmail.com>
2023-01-09 13:45:00 -0700

    Finished Prep Project 3

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -7,7 +7,6 @@ class Program
     
         Console.Write("What is your first name? ");
         string first_name = Console.ReadLine();
-        
         Console.Write("What is your last name? ");
         string last_name = Console.ReadLine();
 
--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -4,6 +4,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Prep3 World!");
-    }
+        int targetNumber;
+        string guess;
+        int guessNumber = 0;
+        Random randomGen = new Random();
+        targetNumber = randomGen.Next(1,100);
+        
+
+        while (guessNumber != targetNumber){
+
+            Console.Write("Guess a number: ");
+            guess = Console.ReadLine();
+            guessNumber = int.Parse(guess);
+            if (guessNumber > targetNumber){
+                Console.WriteLine("Lower");
+            }
+            else if (guessNumber < targetNumber){
+                Console.WriteLine("Higher");
+            }
+        }
+        Console.WriteLine("Congrats!!!");
+    }  
 }
