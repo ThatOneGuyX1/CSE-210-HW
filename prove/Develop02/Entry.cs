4e2707b54d0bffa40099380bf1fe9798d02479f5
ThatOneGuyX1 <iansearle0@gmail.com>
2023-01-30 13:45:33 -0700

    Worked on the devlop 2

---
+++ prove/Develop02/Entry.cs
@@ -0,0 +1,22 @@
+using System;
+using System.IO;
+
+
+
+public class Entry
+{
+    public string _dateOfEntry;
+
+    public string _entryString;
+
+    public string _givenPrompt;
+
+   public void Display()
+   {
+    Console.WriteLine(_dateOfEntry);
+    Console.WriteLine(_givenPrompt);
+    Console.WriteLine(_entryString);
+
+
+   }
+}
