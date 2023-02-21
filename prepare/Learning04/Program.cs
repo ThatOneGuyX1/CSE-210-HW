c4d62bf597c25c87504094031f9d775c6744b862
ThatOneGuyX1 <iansearle0@gmail.com>
2023-02-20 17:03:32 -0700

    Fisnihed prep 4

--- prepare/Learning04/Program.cs
+++ prepare/Learning04/Program.cs
@@ -5,5 +5,22 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Learning04 World!");
+
+        Assignment a1 = new Assignment("Ian Searle", "Math");
+        Console.WriteLine(a1.GetSummary());
+
+
+        MathAssignment m1 = new MathAssignment("Luke", "Fractions", "Section 1.2", "12-34 evens");
+
+        Console.WriteLine(m1.GetSummary());
+        Console.WriteLine(m1.GetHomeworkList());
+
+        WritingAssignment w1 = new WritingAssignment("Sam", "English 101", "The causes of Autisms by Greg");
+
+
+        Console.WriteLine(w1.GetSummary());
+        Console.WriteLine(w1.GetWritingInformation());
+
+
     }
 }
